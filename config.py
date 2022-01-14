4cc43abc42c5ec1b143708f384d0b6f62e299c4e
Boyan A <80381466+BoyanAleksandrov@users.noreply.github.com>
2022-01-14 02:50:28 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Enter username and password of instagram account: 
+username = ''
+password = ''
+
+# Delay time between likes (seconds):
+delay = 10
+
+# Amount of likes for each tag or location:
+amount = 3
+
+# Like by tags - 'tag'. Like by locations - 'location'
+type_like = 'tag'
+# type_like = 'location'
+
+tags = ['bike','bikelife', 'motorcycle', 'motorrad', 'motorcycles', 'nakedbike', 'bikersofinstagram','bikes','race', 'rideout', 'supersport', 'photofraphy', 'sportbikelife','sportsbikelife','motorcyclelife','2wheels','cbr650r','honda','hondacbr650','hondacbr1000rr','motocross','motogirl','motolife','motoventoturkiye','motorcyclemafia','motorcustom']
+
+locations = ['bike','bikelife', 'motorcycle', 'motorrad', 'motorcycles', 'nakedbike', 'bikersofinstagram','bikes','race', 'rideout', 'supersport', 'photofraphy', 'sportbikelife','sportsbikelife','motorcyclelife','2wheels','cbr650r','honda','hondacbr650','hondacbr1000rr','motocross','motogirl','motolife','motoventoturkiye','motorcyclemafia','motorcustom']
